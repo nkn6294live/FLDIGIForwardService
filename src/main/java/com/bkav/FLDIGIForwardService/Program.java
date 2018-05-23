57d15a2db30b4bae3bb411a010def44b92adf92b
NamNK <nkn6294@live.co.uk>
2018-05-23 16:59:03 +0700

    modify

--- src/main/java/com/bkav/FLDIGIForwardService/Program.java
+++ src/main/java/com/bkav/FLDIGIForwardService/Program.java
@@ -3,7 +3,6 @@ package com.bkav.FLDIGIForwardService;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.UnknownHostException;
 
 import com.bkav.FLDIGIForwardService.handler.Handler;
 import com.bkav.FLDIGIForwardService.pack.FLDIGIPackage;
@@ -11,7 +10,7 @@ import com.bkav.FLDIGIForwardService.pack.FLDIGIPackage;
 //{"macaddress":"02:00:00:00:00:00","lat":15.480129811912775,"lng":109.2829299904406};
 
 public class Program {
-	public static void main(String[] args) throws UnknownHostException, IOException { //throws Exception {
+	public static void main(String[] args) {
 		java.net.Socket socketFldigi = null;
 		while (true) {
 			try {
@@ -24,6 +23,9 @@ public class Program {
 						Handler.handlerPackage(pack);
 						SocketIOService.send(pack);						 
 					} catch (IOException ex) {
+						try {
+							Thread.sleep(100);							
+						} catch (InterruptedException e) {}
 					}
 				}
 			} catch (Exception ex) {
